45f792d988c59102e87de593a39dff6f3a3f06ac
jhmaster2000 <32803471+jhmaster2000@users.noreply.github.com>
2021-06-09 09:57:13 -0300

    optimize injector

--- utils/console/inject.js
+++ utils/console/inject.js
@@ -1,7 +1,9 @@
 import util from 'util';
+import { loopAndModifyArray, loopAndModifyObject } from './loopAndModify.js';
 import injectTable from './table.js';
 const UTCDateCallerStr = `function UTCDateCaller\\(\\.\\.\\.ctorParams\\) {(.|\n|\r)*if \\(this === undefined\\) return new UTCDate\\(\\)\\.toString\\(\\);(.|\n|\r)*let aUTCDate = new UTCDate\\(\\.\\.\\.ctorParams\\);(.|\n|\r)*aUTCDate\\.__proto__ = NativeDate\\.prototype;(.|\n|\r)*aUTCDate\\.__proto__\\.constructor = NativeDate\\.prototype;(.|\n|\r)*aUTCDate\\.__proto__\\.__proto__ = this\\.__proto__;(.|\n|\r)*aUTCDate\\.__proto__\\.__proto__\\.constructor = this\\.__proto__;(.|\n|\r)*return aUTCDate;(.|\n|\r)*}`;
 const UTCDateCallerRegex = new RegExp(UTCDateCallerStr, 'g');
+Object.isObject = (obj) => { return ({}).toString.apply(obj) === '[object Object]'; }
 
 const originals = {
     log: console.log,
@@ -19,15 +21,24 @@ export default function inject(method, args) {
     [...args].forEach((arg, idx) => {
         if (typeof arg === 'symbol') return; // Symbols are not supported.
         if (method === 'table' && (idx === 0 || idx === 1)) return args[idx] = injectTable(arg, idx);
-        if (arg instanceof Date) return args[idx] = arg.toISOString();
-        if (typeof arg !== 'object' && util.inspect(arg).includes('[Function: UTCDateCaller]')) {
-            if (typeof arg === 'string') return arg.replace(/\[Function: UTCDateCaller\]/g, '[Function: Date]');
-            else return args[idx] = util.inspect(arg).replace(/\[Function: UTCDateCaller\]/g, '[Function: Date]');
-        }
-        if (typeof arg !== 'object' && util.inspect(arg).match(UTCDateCallerRegex)) {
-            if (typeof arg === 'string') return args[idx] = arg.replace(UTCDateCallerRegex, 'function Date() { [native code] }');
-            else return args[idx] = util.inspect(arg).replace(UTCDateCallerRegex, 'function Date() { [native code] }').slice(1, -1);
-        }
+        if (Array.isArray(arg)) return args[idx] = loopAndModifyArray(arg, loopCondition, loopModifier);
+        if (Object.isObject(arg)) return args[idx] = loopAndModifyObject(arg, loopCondition, loopModifier);
+        args[idx] = loopModifier(arg, 0, args);
     });
     originals[method].apply(console, args);
+}
+
+function loopCondition(x, id, main) { if (!Array.isArray(x) && !Object.isObject(x)) return true; }
+
+function loopModifier(x, id, main) {
+    if (x instanceof Date) return x.toISOString();
+    if (typeof x !== 'object' && util.inspect(x).includes('[Function: UTCDateCaller]')) {
+        if (typeof x === 'string') return x.replace(/\[Function: UTCDateCaller\]/g, '[Function: Date]');
+        else return util.inspect(x).replace(/\[Function: UTCDateCaller\]/g, '[Function: Date]');
+    }
+    if (typeof x !== 'object' && util.inspect(x).match(UTCDateCallerRegex)) {
+        if (typeof x === 'string') return x.replace(UTCDateCallerRegex, 'function Date() { [native code] }');
+        else return util.inspect(x).replace(UTCDateCallerRegex, 'function Date() { [native code] }').slice(1, -1);
+    }
+    return x;
 }
