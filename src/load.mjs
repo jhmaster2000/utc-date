30b6acb026e009b0c21e4c9c49d572b281103f16
jhmaster2000 <32803471+jhmaster2000@users.noreply.github.com>
2022-03-26 05:14:37 -0300

    oops that shouldnt get commited

--- src/load.mjs
+++
@@ -1,7 +0,0 @@
-import DateProxy, { NativeDateSymbol, UTCDateSymbol } from './proxy.js';
-Date = DateProxy;
-const NativeDate = DateProxy[NativeDateSymbol];
-function isUTCDate(date) {
-    return date[UTCDateSymbol] ?? false;
-}
-export { DateProxy as UTCDate, NativeDate, isUTCDate };
